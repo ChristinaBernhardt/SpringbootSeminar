4a8c23d01f29cc67f0a004f39031556c544c2cce
BERNHARDT Christina <christina.bernhardt@soprasteria.com>
2025-11-19 14:32:20 +0100

    Second commit

---
+++ src/main/java/Devs/SpringbootSeminar/SpringbootSeminarApplication.java
@@ -0,0 +1,13 @@
+package Devs.SpringbootSeminar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSeminarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSeminarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Devs/SpringbootSeminar/SpringbootSeminarApplicationTests.java
@@ -0,0 +1,13 @@
+package Devs.SpringbootSeminar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSeminarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
